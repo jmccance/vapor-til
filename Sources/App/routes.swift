32df7b175f479dd9f3773afd21053653f5303c8a
Joel McCance <joel@spothero.com>
2019-06-26 08:21:58 -0500

    Implementation up to Chapter 7

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,10 +11,51 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
-
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+    
+    router.post("api", "acronyms") { req in
+        return try req.content.decode(Acronym.self)
+            .flatMap { acronym in
+                return acronym.save(on: req)
+        }
+    }
+    
+    router.get("api", "acronyms", Acronym.parameter) { (req: Request) -> Future<Acronym> in
+//        guard let id: Int = req.parameters.next(Int.self) else {
+//            return req.future(nil)
+//        }
+        
+//        do {
+//            return try Acronym.find(0, on: req)
+//        } catch {
+//            return req.future(nil)
+//        }
+        
+        return req.future(Acronym(short: "rtfm", long: "red trolls fight madly"))
+        
+        
+//        return Acronym.query(on: req).all()
+    }
+    
+    router.get("api", "acronyms") { req -> Future<[Acronym]> in
+        return Acronym.query(on: req).all()
+    }
+    
+    router.put("api", "acronyms", Acronym.parameter) { req in
+        return try flatMap(
+            to: Acronym.self,
+            req.parameters.next(Acronym.self),
+            req.content.decode(Acronym.self)
+        ) { acronym, updatedAcronym -> Future<Acronym> in
+                acronym.short = updatedAcronym.short
+                acronym.long = updatedAcronym.long
+                
+                return acronym.save(on: req)
+        }
+    }
+    
+    router.delete("api", "acronyms", Acronym.parameter) { req -> Future<HTTPStatus> in
+        return try req.parameters.next(Acronym.self)
+            .delete(on: req)
+            .transform(to: .noContent)
+    }
 }
